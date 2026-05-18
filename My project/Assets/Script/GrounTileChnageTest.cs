2a0304284d9987d10c923fe7515e3b9dafd6f5ab
wlwodnr <wlwodnr0111@gmail.com>
2026-05-18 18:38:54 +0900

    감자 리소스 및 코드 준비
    리소스 올기 및 타일형으로 변형

--- My project/Assets/Script/GrounTileChnageTest.cs
+++ My project/Assets/Script/GrounTileChnageTest.cs
@@ -5,25 +5,32 @@ using System.Collections.Generic;
 
 public class GrounTileChnageTest : MonoBehaviour
 {
-    [SerializeField] Tilemap targetTile;
-    [SerializeField]private TileBase targetChange;
+    [SerializeField]Tilemap targetTile;
+    [SerializeField]private TileBase PlowedTile;
+    [SerializeField] private TileBase WetTile;
 
+    [SerializeField]private List<Vector3Int> collectedTilePositions = new List<Vector3Int>();
 
-    [SerializeField]private List<GameObject> collectedGameObjectTiles = new List<GameObject>();
 
     void Start()
     {
         Debug.Log("타일 변경 시작");
-        CollectGameObjectTiles();
-        ChangeAllCollectedTiles();
+        CollectGameObjectTilePositions();
     }
 
     void Update()//
     {
-        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            ChangeAllCollectedTiles();
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            WateringInTile();
+        }
     }
 
-    public void CollectGameObjectTiles()
+    public void CollectGameObjectTilePositions()
     {
         if(targetTile  == null)
         {
@@ -31,47 +38,77 @@ public class GrounTileChnageTest : MonoBehaviour
             return;
         }
 
-        collectedGameObjectTiles.Clear();
+        collectedTilePositions.Clear();
 
-        foreach(Transform child in targetTile.transform)
+        foreach (Transform child in targetTile.transform)
         {
-            collectedGameObjectTiles.Add(child.gameObject);
+            Vector3Int cellPosition = targetTile.WorldToCell(child.position);
+            collectedTilePositions.Add(cellPosition);
         }
-        Debug.Log($"총 {collectedGameObjectTiles.Count}개의 자식 오브젝트 타일을 찾았습니다");
+        Debug.Log($"총 {collectedTilePositions.Count}개의 자식 오브젝트 타일을 찾았습니다");
     }
 
 
     public void ChangeAllCollectedTiles()
     {
-        if (targetChange == null)
+        if (PlowedTile == null)
         {
-            Debug.Log("타일맵 또는 변경할 targetChange 타일이 설정되지 않았습니다.");
+            Debug.Log("타일맵 또는 변경할 PlowedTile 타일이 설정되지 않았습니다.");
             return;
         }
-        if(collectedGameObjectTiles.Count == 0 )
+        if(collectedTilePositions.Count == 0 )
         {
             Debug.Log("변경할 오브젝트 타일이 리스트에 없습니다.");
             return;
         }
+        List<GameObject> toDestroy = new List<GameObject>();
 
-        foreach(GameObject tileObj in collectedGameObjectTiles)
+        foreach(Transform child in targetTile.transform)
         {
-            if(tileObj == null)
-            {
-                continue;
-            }
-            Vector3Int cellPosition = targetTile.WorldToCell(tileObj.transform.position);
-
-            targetTile.SetTile(cellPosition, targetChange);
-
-            Destroy(tileObj);
+            toDestroy.Add(child.gameObject);
+        }
+        foreach(GameObject obj in toDestroy)
+        {
+            Destroy(obj);
+        }
+        foreach(Vector3Int pos in collectedTilePositions)
+        {
+            targetTile.SetTile(pos, PlowedTile);
+        }
+        Debug.Log($"{collectedTilePositions.Count}개의 구역을 개간했습니다");
+    }
+    public void WateringInTile()
+    {
+        if (WetTile == null || targetTile == null)
+        {
+            Debug.Log("타일맵 또는  WetTile 타일이 설정되지 않았습니다.");
+            return;
+        }
+        if(collectedTilePositions.Count == 0)
+        {
+            Debug.Log("탐색된 타일 좌표가 리스트에 없습니다.");
         }
 
-        Debug.Log($"{collectedGameObjectTiles.Count}개의 오브젝트 타일을 변경");
+        int wateredCount = 0;
 
-        collectedGameObjectTiles.Clear();
+        foreach(Vector3Int pos in collectedTilePositions)
+        {
+            TileBase currentTile = targetTile.GetTile(pos);
+
+            if(currentTile == PlowedTile)
+            {
+                targetTile.SetTile(pos, WetTile);
+                wateredCount++;
+            }
+            else
+            {
+                Debug.Log("현재 타일을 개간하지 않았습니다.");
+                return;
+            }
+            Debug.Log($"{wateredCount}개의 개간된 땅에 물을 뿌렸습니다");
+        }
 
     }
-    
+
 
 }
