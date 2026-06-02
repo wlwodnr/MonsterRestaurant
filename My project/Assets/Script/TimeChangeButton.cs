67da2cd69bdd99fbb70e5de13708490381481d7a
wlwodnr <wlwodnr0111@gmail.com>
2026-06-02 16:30:53 +0900

    데이터 드리븐 수정 및 코인 플립 에니메이션 추가중
    강사님의 데이터드리븐 피드백 반영하여 수정
    코인 플립 에니메이션 작업중
    TimeManager와 연결하지 않음

---
+++ My project/Assets/Script/TimeChangeButton.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeChangeButton : MonoBehaviour
+{
+    [SerializeField]
+    private Button Button_Base;
+    [SerializeField]
+    private Animator Animator_Coin;
+
+    private Action _onTimeChangeCompleteCallback;
+    private bool _isAnimating = false;
+
+    private void Awake()
+    {
+        if(Button_Base == null )
+        {
+            Button_Base = GetComponent<Button>();
+        }
+        if( Animator_Coin == null )
+        {
+            Animator_Coin = GetComponent<Animator>();
+        }
+
+        Button_Base.onClick.RemoveAllListeners();
+        Button_Base.onClick.AddListener(OnClickTimeChangeButton);
+    }
+
+    private void OnDisable()
+    {
+        Button_Base.onClick.RemoveAllListeners();
+    }
+
+    public void BindOnTimeChangeCompleteEvent(Action onCompleteCallback)
+    {
+        this._onTimeChangeCompleteCallback = onCompleteCallback;
+    }
+
+    private void OnClickTimeChangeButton()
+    {
+        if(_isAnimating)
+        {
+            return;
+        }
+        bool isCurrentlyMorning = Animator_Coin.GetBool("IsMorning");
+
+        StartCoroutine(PlayerTransitionRoutine(isCurrentlyMorning));
+    }
+
+    private IEnumerator PlayerTransitionRoutine(bool isCurrentlyMorning)
+    {
+        _isAnimating = true;
+        string targetStateName = "";
+
+        if(isCurrentlyMorning )
+        {
+            Animator_Coin.SetTrigger("OnChangedToNight");
+            Animator_Coin.SetBool("IsMorning", false);
+            targetStateName = "TimeSetNight";
+        }
+        else
+        {
+            Animator_Coin.SetTrigger("OnChangedToMorning");
+            Animator_Coin.SetBool("IsMorning", true);
+            targetStateName = "TimeSetMorning";
+        }
+
+        yield return null;
+
+        var stateInfo = Animator_Coin.GetCurrentAnimatorStateInfo(0);
+        if (stateInfo.IsName(targetStateName))
+        {
+            yield return new WaitForSeconds(stateInfo.length);
+        }
+        else
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        _isAnimating = false;
+
+        _onTimeChangeCompleteCallback?.Invoke();
+    }
+
+
+
+
+
+}
