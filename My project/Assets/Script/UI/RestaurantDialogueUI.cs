17654fb3727efea0fb4a12f41ca102ea078af993
wlwodnr <wlwodnr0111@gmail.com>
2026-06-04 16:05:56 +0900

    고객 데이터 수정 및 다이어로그 만드는 중
    고객 데이터의 Name값을 도깨비의 주문으로 통일
    Dialogue 구현 중
    파일 경로 수정

---
+++ My project/Assets/Script/UI/RestaurantDialogueUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class RestaurantDialogueUI : MonoBehaviour
+{
+    private Text Text_DialogueDescription;
+    private Text Text_CustomerName;
+    private Button Button_Next;
+
+    private void Awake()
+    {
+        if(Button_Next != null)
+        {
+            Button_Next.onClick.RemoveAllListeners();
+        }
+    }
+
+    public void SetDialogue(CustomerData customerData)
+    {
+        if(customerData == null)
+        {
+            return;
+        }
+        if(Text_CustomerName != null)
+        {
+            Text_CustomerName.text = customerData.Name;
+        }
+        if (Text_DialogueDescription != null)
+        {
+            Text_DialogueDescription.text = customerData.Description;
+        }
+    }
+
+}
