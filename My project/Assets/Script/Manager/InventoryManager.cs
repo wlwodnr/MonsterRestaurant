1d66591bae6f270976e4a5a26429990c6cef9bca
wlwodnr <wlwodnr0111@gmail.com>
2026-06-05 17:03:06 +0900

    빌드 오류 수정

--- My project/Assets/Script/Manager/InventoryManager.cs
+++ My project/Assets/Script/Manager/InventoryManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance { get; private set; }
@@ -25,6 +24,50 @@ public class InventoryManager : MonoBehaviour
             return;
         }
     }
+
+    //public void AddItem(string itemId, int count)
+    //{
+    //    if(string.IsNullOrEmpty(itemId))
+    //    {
+    //        Debug.Log("들어오는 아이템의 id값이 없습니다.");
+    //        return;
+    //    }
+    //    if(itemInventory.ContainsKey(itemId))
+    //    {
+    //        itemInventory[itemId] += count;
+    //    }
+    //    else
+    //    {
+    //        itemInventory[itemId] = count;
+    //    }
+
+    //        Debug.Log($"[Inventory] 아이템 획득: {itemId}을(를) {count}만큼 획득하여 총 {itemInventory[itemId]}개가 되었습니다.");
+
+    //}
+
+    //public bool RemoveItem(string itemId, int count)
+    //{
+    //    if(!itemInventory.ContainsKey(itemId) || itemInventory[itemId] < count)
+    //    {
+    //        Debug.LogWarning($"[Inventory] {itemId}의 수량이 부족하여 소모할 수 없습니다.");
+    //        return false;
+    //    }
+
+    //    itemInventory[itemId] -= count;
+    //    Debug.Log($"[Inventory] {itemId} 소모: -{count} (현재 수량: {itemInventory[itemId]})");
+
+    //    return true;
+    //}
+
+    //public int GetItemCount(string itemId)
+    //{
+    //    if (itemInventory.ContainsKey(itemId))
+    //    {
+    //        return itemInventory[itemId];
+    //    }
+    //    Debug.Log("해당 아이템은 없습니다.");
+    //    return 0;
+    //}
 
     public void RefreshInventoryUI()
     {
